2dbb598938e5775cb302203a92967c102ef83b8f
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2022-12-24 13:54:32 +0100

    feat: add sort select

--- online-shop/src/pages/MainPage/MainPage.tsx
+++ online-shop/src/pages/MainPage/MainPage.tsx
@@ -1,15 +1,27 @@
 import ProductCard from "components/ProductCard/ProductCard";
-import React from "react";
+import React, { useState } from "react";
 import { useGetProductsQuery } from "redux/fakestore/fakestore.api";
 import "./MainPage.css";
 
 const MainPage = () => {
-  const { data, isError, isLoading } = useGetProductsQuery("asc");
+  const [sort, setSort] = useState<string>("asc");
+  const { data, isError, isLoading } = useGetProductsQuery(sort);
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    e.preventDefault();
+    setSort(e.target.value);
+    console.log(e.target.value);
+  };
+
   return (
     <div className="main">
       <div className="main__contant">
         {isLoading && <p>Loading...</p>}
-        <div className="pagination"></div>
+        <div className="pagination">
+          <select name="sort" id="" onChange={(e) => handleChange(e)}>
+            <option value="asc">A-Z</option>
+            <option value="desc">Z-A</option>
+          </select>
+        </div>
         <div className="main__contant">
           <ul className="products">
             {data &&
