c7b240e7b6ff5595a2168b67a23e29df6191a2bc
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2022-12-25 22:09:21 +0100

    feat: create ErrorPage

--- online-shop/src/pages/ErrorPage/ErrorPage.tsx
+++ online-shop/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./ErrorPage.css";
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="error__container">
+      <h3 className="error__title">Something went wrong....</h3>
+    </div>
+  );
 };
 
 export default ErrorPage;
