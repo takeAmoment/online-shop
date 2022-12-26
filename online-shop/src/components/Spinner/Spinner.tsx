8b11029d104ab2f3b8e37fb59a2bc4f4ee11f054
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2022-12-26 17:17:11 +0100

    feat: create Spinner

---
+++ online-shop/src/components/Spinner/Spinner.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Spinner.css";
+
+const Spinner = () => {
+  return (
+    <div className="spinner__block">
+      <span className="loader"></span>
+    </div>
+  );
+};
+
+export default Spinner;
