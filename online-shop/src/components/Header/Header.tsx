9da6cf182604bb21efea015e0c43117487d39f60
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2022-12-28 20:47:25 +0100

    feat: add menu burger to header

--- online-shop/src/components/Header/Header.tsx
+++ online-shop/src/components/Header/Header.tsx
@@ -1,14 +1,28 @@
 import { useAppSelector } from "hooks/useSelector";
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
   const { cart } = useAppSelector((state) => state.cart);
+  const [isBurgerActive, setBurgerActive] = useState<boolean>(false);
+  const [isMenuActive, setIsMenuActive] = useState<boolean>(false);
+
+  const handleClick = () => {
+    console.log("click");
+    if (isBurgerActive) {
+      setBurgerActive(false);
+      setIsMenuActive(false);
+    } else {
+      setBurgerActive(true);
+      setIsMenuActive(true);
+    }
+  };
+
   return (
     <header className="header">
       <div className="header__logo">EShop</div>
-      <nav className="header__menu">
+      <nav className={isMenuActive ? "header__menu active" : "header__menu"}>
         <ul className="menu__list">
           <li className="list__link">
             <NavLink to="/" className="link">
@@ -35,6 +49,11 @@ const Header = () => {
       <NavLink to="/cart" className="cart">
         <span className="cart__amount">{cart.length}</span>
       </NavLink>
+      <div className="menu__burger" onClick={handleClick}>
+        <span className={isBurgerActive ? "line1 active" : "line1"}></span>
+        <span className={isBurgerActive ? "line2 active" : "line2"}></span>
+        <span className={isBurgerActive ? "line3 active" : "line3"}></span>
+      </div>
     </header>
   );
 };
