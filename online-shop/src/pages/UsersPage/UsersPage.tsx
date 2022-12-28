3aba36e771736e99824fff86f1cf8478eff622e8
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2022-12-28 13:38:13 +0100

    feat: add Spinner and ErrorPage to UsersPage

--- online-shop/src/pages/UsersPage/UsersPage.tsx
+++ online-shop/src/pages/UsersPage/UsersPage.tsx
@@ -1,10 +1,18 @@
+import Spinner from "components/Spinner/Spinner";
 import UserCard from "components/UserCard/UserCard";
+import ErrorPage from "pages/ErrorPage/ErrorPage";
 import React from "react";
 import { useGetUsersQuery } from "redux/fakestore/fakestore.api";
 import "./UsersPage.css";
 
 const UsersPage = () => {
-  const { data, isLoading } = useGetUsersQuery();
+  const { data, isLoading, isError } = useGetUsersQuery();
+  if (isLoading) {
+    return <Spinner />;
+  }
+  if (isError) {
+    return <ErrorPage />;
+  }
   return (
     <div className="users__container">
       <h2 className="title">Users</h2>
