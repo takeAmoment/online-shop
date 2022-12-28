d6f4093d21955e8732afd806faa1f50967b619d0
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2022-12-28 16:16:05 +0100

    feat: add error message to Product

--- online-shop/src/components/Product/Product.tsx
+++ online-shop/src/components/Product/Product.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import { useGetProductQuery } from "redux/fakestore/fakestore.api";
+import { List } from "react-content-loader";
 
 interface ProductProps {
   productId: number;
@@ -7,15 +8,26 @@ interface ProductProps {
 
 const Product: FC<ProductProps> = ({ productId }) => {
   const { data, isLoading, isError } = useGetProductQuery(productId);
+  if (isError) {
+    return (
+      <h4 className="error__message">
+        Something went wrong.... This product was not found.
+      </h4>
+    );
+  }
   return (
     <div className="cart__product__card">
-      <div className="product__card__left">
-        <img src={data?.image} alt={data?.title} className="image_small" />
-        <ul className="product__card__description">
-          <li className="description__title">{data?.title}</li>
-          <li className="price">{data?.price}$</li>
-        </ul>
-      </div>
+      {isLoading ? (
+        <List />
+      ) : (
+        <div className="product__card__left">
+          <img src={data?.image} alt={data?.title} className="image_small" />
+          <ul className="product__card__description">
+            <li className="description__title">{data?.title}</li>
+            <li className="price">{data?.price}$</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
