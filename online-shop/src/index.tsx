3c4321374e463180fa0fd28ee6b3fd04ff20fb2c
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2022-12-29 20:20:11 +0100

    feat: add basename to BrowserRouter

--- online-shop/src/index.tsx
+++ online-shop/src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="online-shop">
     <Provider store={store}>
       <App />
     </Provider>
